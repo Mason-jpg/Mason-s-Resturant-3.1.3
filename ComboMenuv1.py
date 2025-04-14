32ed107b3008d6b57e3b6d87a44dc9faa8e6fc63
Mason-jpg <masonteki1114@gmail.com>
2025-04-14 12:38:43 -0700

    Update ComboMenuv1-done.py

--- ComboMenuv1.py
+++ ComboMenuv1.py
@@ -1,3 +1,7 @@
 '''Iteration 1
  Ask the user for a type of sandwich (chicken $5.25, beef $6.25, tofu $5.75).
  Have the program output the user’s sandwich selection to verify that the program is working correctly.'''
+def startUp():
+    greeting = input("Welcome to Mason's Resturant! What would you like to order? \n Options: \n -Chicken 5.25 \n -Beef 6.75 \n -Tofu 5.75 \n > " )  
+    print (greeting)
+startUp()
