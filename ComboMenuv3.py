fe7180e5a56b38bf4ac72f2fe6a074efd19d868c
Mason-jpg <masonteki1114@gmail.com>
2025-04-18 12:14:12 -0700

    Update ComboMenuv3complete.py

--- ComboMenuv3.py
+++ ComboMenuv3.py
@@ -10,3 +10,59 @@ Add the following features to your first iteration:
  If the user inputs yes to mega-size, give them large fries at the large fries price instead of their small fries.
  Have the program output the user’s fries selection to verify that the program is working correctly.
  Adjust the program so the total cost only outputs to the user after their sandwich, drink, and fries selection.'''
+def startUp():
+    greeting = input("Welcome to Mason's Resturant! What would you like to order? \n Options: \n -Chicken 5.25 \n -Beef 6.75 \n -Tofu 5.75 \n > " )  
+   
+    price = 0
+    if greeting == "Chicken" or greeting == "chicken" or greeting == "CHICKEN":
+        price = 5.25
+        print(greeting, "? ")
+    elif greeting == "Beef" or greeting == "beef" or greeting == "BEEF":
+        price = 6.75
+        print(greeting, "? ")
+    elif greeting == "Tofu" or greeting == "tofu" or greeting == "TOFU":
+        price = 5.75
+        print(greeting, "? ")
+    else :
+            price = 0
+            print("Invaild answer! please check what you've typed!")
+            return 0 
+
+    askfordrink = input("Would you like a drink? > ")
+    if askfordrink == "yes" :
+           print( "Options: \n -Small 1.00 \n -Medium 1.75 \n -Large 2.25 \n > ")
+           askfordrink = input("What size? > ")
+    if askfordrink == "Small" or askfordrink == "small" or askfordrink == "SMALL":
+        price = price + 1
+        print(askfordrink, "? ")
+    elif askfordrink == "Medium" or askfordrink == "medium" or askfordrink == "MEDIUM":
+        price = price + 1.75
+        print(askfordrink, "? ")
+    elif askfordrink == "Large" or askfordrink == "large" or askfordrink == "LARGE":
+        price = price + 2.25
+        print(askfordrink, "? ")
+    else :
+            price = 0
+            print("Invaild answer! please check what you've typed!")
+            return 0 
+   
+    askforfries = input("Would you like fries with your meal? > ")
+    if askforfries == "yes" :
+           print( "Options: \n -Small 1.00 \n -Medium 1.75 \n -Large 2.25 \n > ")
+           askforfries = input("What size? > ")
+    if askforfries == "Small" or askforfries == "small" or askforfries == "SMALL":
+        price = price + 1
+        askforfries = input("Would you like to supersize? \n > ")
+        if askforfries == "yes":
+             price = price + -1 + 2
+        print(askforfries, "? " , "\n subtotal: $" , str((price)) + "0")
+    elif askforfries == "Medium" or askforfries == "medium" or askforfries == "MEDIUM":
+        price = price + 1.50
+        print(askforfries, "? " , "\n subtotal: $" , str((price)) + "0")
+    elif askforfries == "Large" or askforfries == "large" or askforfries == "LARGE":
+        price = price + 2
+        print(askforfries, "? " , "\n subtotal: $" , str((price)) + "0")
+    else :
+            price = 0
+            print("Invaild answer! please check what you've typed!")
+            return 0 
